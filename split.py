b48964d2572aa11bc58174531426e4ce097bfc45
Lucas Tindall <ltindall@ucsd.edu>
2017-06-03 22:16:02 -0700

    Changed vanillaConvnet to work with plankton data. Added split.py script to split initial dataset into train and test data. Roughly 80/20 split.

---
+++ split.py
@@ -0,0 +1,49 @@
+import os
+import shutil
+from random import shuffle
+
+src_dir = '/var/home/l_ltindall/plankton_images'
+
+if not os.path.exists(src_dir+'/keras_images/train'):
+    os.makedirs(src_dir+'/keras_images/train')
+
+if not os.path.exists(src_dir+'/keras_images/test'):
+    os.makedirs(src_dir+'/keras_images/test')
+
+src_dirs = os.listdir(src_dir+'/all_images')
+print src_dirs
+total_files = 0
+for cat in src_dirs: 
+    if os.path.isdir(src_dir+'/all_images/'+cat): 
+        #print 'files in ',cat
+
+        files = os.listdir(src_dir+'/all_images/'+cat)
+        #print len(files)
+        total_files = total_files + len(files)
+   
+	#print files[:10]
+        shuffle(files)
+        #print "vs."
+        #print files[:10]
+	
+	os.makedirs(src_dir+'/keras_images/train/'+cat)
+        os.makedirs(src_dir+'/keras_images/test/'+cat)
+        
+        i = 0
+        for f in files: 
+            if i >230: 
+                shutil.copy(src_dir+'/all_images/'+cat+'/'+f,src_dir+'/keras_images/train/'+cat+'/'+f)
+            else: 
+                shutil.copy(src_dir+'/all_images/'+cat+'/'+f,src_dir+'/keras_images/test/'+cat+'/'+f)
+            i = i + 1
+        #print os.listdir(src_dir+'/'+cat)
+        
+
+print total_files
+'''
+for file_name in src_files:
+    full_file_name = os.path.join(src, file_name)
+    if (os.path.isfile(full_file_name)):
+        shutil.copy(full_file_name, dest)
+
+'''
